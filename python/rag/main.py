38bcbaf0d68f3f044b1ab0778ca4063f75b44120
henrty <dinhductai2004@gmail.com>
2026-03-14 17:17:52 +0700

    feat/update-port

--- python/rag/main.py
+++ python/rag/main.py
@@ -188,6 +188,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=int(os.getenv("PORT", 8000)),
+        port=int(os.getenv("PORT", 10000)),
         reload=False,
     )
